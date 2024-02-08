1f11489a9bf05975bc995c70ee343c8651e23670
Hanh <hanh425@gmail.com>
2024-02-08 11:09:16 +1000

    Remove vagrant

--- zcash-build.sh
+++ zcash-build.sh
@@ -1,8 +1,8 @@
+set -x
 sudo apt-get update
 sudo apt-get install -y git bc bison flex libssl-dev make libc6-dev libncurses5-dev
-git clone https://github.com/zcash/zcash.git
 sudo apt-get install -y crossbuild-essential-arm64
 sudo apt-get install -y libtinfo5 automake libtool pkg-config
+git clone -b $1 --depth 1 https://github.com/zcash/zcash.git
 cd zcash
-git checkout $1
 HOST=aarch64-linux-gnu ./zcutil/build.sh
